b84e65f0ad94a72ef30d09c2ddbf784fd190b2dc
Solo-steven <ssteven19@kimo.com>
2023-07-29 16:19:42 +0800

    feat: add js becnchmark

---
+++ js-benchmark/index.js
@@ -0,0 +1,54 @@
+const { Bench } = require('tinybench');
+const fs = require('fs');
+const path = require('path');
+const bench = new Bench({ time: 250 });
+const { parse, stringify } = require('postcss');
+const tokenizer = require('postcss/lib/tokenize');
+
+const tinyFileString =  fs.readFileSync(path.join(__dirname, "../assets/bootstrap-rebot.css")).toString();
+const biggerFileString =  fs.readFileSync(path.join(__dirname, "../assets/bootstrap.css")).toString();
+const hugeFileString =  fs.readFileSync(path.join(__dirname, "../assets/tailwind-dark.css")).toString();
+async function benchFun() {
+    // let tinyFileAST = parse(tinyFileString);;
+    // let biggerFileAST = parse(biggerFileString);
+    // let hugeFileAST = parse(hugeFileString);
+    bench
+    .add('postcss tokenize tiny file', () => {
+        let t = tokenizer({css:tinyFileString});
+        while (t.nextToken());
+    })
+    .add('postcss tokenize bigger file', () => {
+        let t = tokenizer({css:biggerFileString});
+        while (t.nextToken());
+    })
+    .add('postcss tokenize huge file', () => {
+        let t = tokenizer({css:hugeFileString});
+        while (t.nextToken());
+    })
+    .add('postcss parse tiny file', () => {
+        parse(tinyFileString);
+    })
+    .add('postcss parse bigger file', () => {
+        parse(biggerFileString);
+    })
+    .add('postcss parse huge file', () => {
+        parse(hugeFileString);
+    })
+    // .add('postcss generate tiny file', () => {
+    //     let result = "";
+    //     stringify(tinyFileAST, i => { result += i });
+    // })
+    // .add('postcss generate bigger file', () => {
+    //     let result = "";
+    //     stringify(biggerFileAST, i => { result += i });
+    // })
+    // .add('postcss generate huge file', () => {
+    //     let result = "";
+    //     stringify(hugeFileAST, i => { result += i });
+    // })
+
+    await bench.run();
+    console.table(bench.table());
+}
+
+benchFun();
