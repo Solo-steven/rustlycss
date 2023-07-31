1426af7caa3d04369507e1cf903aea1ba9707513
Solo-steven <ssteven19@kimo.com>
2023-07-31 23:49:12 +0800

    feat: implement nested plugin

--- crates/playground/src/main.rs
+++ crates/playground/src/main.rs
@@ -1,14 +1,30 @@
 use rustlycss_parser::parser::Parser;
+use rustlycss_nested::NestedVisitor;
+use rustlycss_codegen::Generator;
 use rustlycss_types::config::GeneralConfig;
 
 fn main() {
     let code = "
-        .test {
-            color: red;
+        .wrapper {
+            @media screen and (max-width: 500px) {
+                color: red;
+                @media screen and (max-width: 300px) {
+                    .container {
+                        color: red;
+                    }
+                }
+                .item {
+                    flex: 1;
+                }
+            }
         }
     ";
-    let config = GeneralConfig::from(true, true);
+    let config = GeneralConfig::from(true, false);
     let mut parser = Parser::new(code, &config);
-    let root = parser.parse();
-    println!("{:?}", root);
+    let mut root = parser.parse();
+    let mut visitor = NestedVisitor::new();
+    visitor.visit(&mut root);
+    let mut gen = Generator::new(&config);
+    gen.generate(&root);
+    println!("{:?}", gen.output);
 }
